6ee889f043d051265c964a94f3e17c9a51f7918b
Nikola <nikola211223232529321@gmail.com>
2025-08-10 23:20:00 +0200

    what sets us apart section complted

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import BannerComponent from "@/components/BannerComponent";
+import OurDriveSection from "@/components/OurDriveSection";
 import PrimaryButton from "@/components/PrimaryButton";
 import SecondaryButton from "@/components/SecondaryButton";
 import TitleComponent from "@/components/TitleComponent";
 import deliverImg1 from "../../public/assets/pohots/about-us/1.jpg";
 import deliverImg2 from "../../public/assets/pohots/about-us/2.jpg";
+import gridImg1 from "../../public/assets/pohots/about-us/3.jpg";
+import gridImg2 from "../../public/assets/pohots/about-us/4.jpg";
+import gridImg3 from "../../public/assets/pohots/about-us/5.jpg";
 
 export default function About() {
 	return (
@@ -37,7 +41,7 @@ export default function About() {
 								transparency, accountability, and a commitment to excellence at
 								every mile.
 							</p>
-							<div className="flex flex-col sm:flex-row gap-3 sm:gap-4 pt-8">
+							<div className="flex flex-col sm:flex-row gap-3 sm:gap-4 pt-6 lg:pt-8">
 								<div className="w-full sm:w-fit">
 									<PrimaryButton text="Ship With Us" link="/contact" />
 								</div>
@@ -54,6 +58,67 @@ export default function About() {
 							<Image
 								src={deliverImg2}
 								alt="deliver image"
+								className="h-full w-full object-cover"
+							/>
+						</div>
+					</div>
+				</div>
+			</section>
+			<OurDriveSection />
+			<section className="py-6 sm:py-12 md:py-16 lg:py-24">
+				<div className="container mx-auto px-4 lg:px-6">
+					<div className="grid grid-cols-2 gap-y-6 gap-x-3 lg:gap-4 2xl:gap-6">
+						<div className="h-full w-full rounded-lg lg:rounded-2xl overflow-hidden max-sm:aspect-square">
+							<Image
+								src={gridImg1}
+								alt="image"
+								className="h-full w-full object-cover"
+							/>
+						</div>
+						<div className="h-full w-full rounded-lg lg:rounded-2xl overflow-hidden max-sm:aspect-square">
+							<Image
+								src={gridImg2}
+								alt="image"
+								className="h-full w-full object-cover"
+							/>
+						</div>
+						<div className="lg:py-10 2xl:py-16 col-span-2 lg:col-span-1">
+							<TitleComponent
+								title="Freight Done Right"
+								subtitle="What Sets Us Apart"
+							/>
+							<p className="text-body pt-4 lg:pt-6 2xl:pt-8">
+								With over a decade of experience and a versatile fleet, we haul
+								a wide range of cargo including dry van, flatbed, step deck, and
+								Conestoga shipments. Every load is backed by real-time tracking,
+								professional dispatch, and a 24/7 maintenance team that keeps
+								our equipment road-ready and reliable.
+							</p>
+							<p className="text-body pt-2 lg:pt-4">
+								At Delta Carrier Group, we operate on a 100% owner-operator
+								model that puts drivers first. There’s no forced
+								dispatch—ever—and we offer transparent pay, with 88% of gross
+								revenue going directly to the driver. Our team provides 24/5
+								dispatch support, including Saturdays, to keep operations
+								running smoothly. We serve all 48 contiguous states with full
+								ELD compliance and alignment with FMCSA regulations. Whether
+								you're a shipper seeking reliable freight solutions or a driver
+								looking for true independence, Delta is the partner you can
+								count on—mile after mile.
+							</p>
+							<div className="flex flex-col sm:flex-row gap-3 sm:gap-4 pt-6 lg:pt-8">
+								<div className="w-full sm:w-fit">
+									<PrimaryButton text="Ship With Us" link="/contact" />
+								</div>
+								<div className="w-full sm:w-fit">
+									<SecondaryButton text="Talk With Our Team" link="/contact" />
+								</div>
+							</div>
+						</div>
+						<div className="h-full w-full rounded-xl lg:rounded-2xl overflow-hidden col-span-2 lg:col-span-1">
+							<Image
+								src={gridImg3}
+								alt="image"
 								className="h-full w-full object-cover"
 							/>
 						</div>
---
+++ components/OurDriveSection.tsx
@@ -0,0 +1,9 @@
+const OurDriveSection = () => {
+	return (
+		<section className="bg-dark py-6 sm:py-10 lg:py-16 2xl:py-[72px]">
+			<div className="container mx-auto px-4 lg:px-6"></div>
+		</section>
+	);
+};
+
+export default OurDriveSection;
