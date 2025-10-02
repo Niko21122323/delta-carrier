afa2cc0686442d4e3846b12f73a37bcf2f3b0f41
Nikola <nikola211223232529321@gmail.com>
2025-10-02 12:08:15 +0200

    rain widget updated

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -1,22 +1,22 @@
 import BannerComponent from "@/components/BannerComponent";
 
 export default function page() {
-	return (
-		<main>
-			<BannerComponent
-				title="Live Weather Forecast"
-				text="Drivers, here you can check the road conditions by switching between wind, clouds, temperature, rain/snow, waves and pressure. Remember! No load is that important for risking your life!"
-			/>
-			<section className="py-12 md:py-16 lg:py-24">
-				<div className="container mx-auto px-4 lg:px-6">
-					<div className="max-w-[1134px] mx-auto">
-						<iframe
-							className="h-full w-full aspect-square sm:aspect-video"
-							src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=4&overlay=wind&product=ecmwf&level=surface&lat=38.686&lon=-97.383"
-						></iframe>
-					</div>
-				</div>
-			</section>
-		</main>
-	);
+  return (
+    <main>
+      <BannerComponent
+        title="Live Weather Forecast"
+        text="Drivers, here you can check the road conditions by switching between wind, clouds, temperature, rain/snow, waves and pressure. Remember! No load is that important for risking your life!"
+      />
+      <section className="py-12 md:py-16 lg:py-24">
+        <div className="container mx-auto px-4 lg:px-6">
+          <div className="max-w-[1134px] mx-auto">
+            <iframe
+              className="h-full w-full aspect-square sm:aspect-video"
+              src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=5&overlay=rain&product=ecmwf&level=surface&lat=41.771&lon=25.4"
+            ></iframe>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
